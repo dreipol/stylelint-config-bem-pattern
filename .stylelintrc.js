372fd0da4a1b9189f95b31034407e881d3c1075c
Gianluca Guarini <gianluca.guarini@gmail.com>
2018-09-04 14:16:52 +0200

    Initial commit

---
+++ .stylelintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  'plugins': [
+    'stylelint-selector-bem-pattern',
+  ],
+  'rules': {
+    'plugin/selector-bem-pattern': {
+      'implicitComponents': [
+        '**/js/app/framework/**/*',
+        '**/scss/app/structure/**/*',
+      ],
+      'componentName': '^[a-z]+(?:-[a-z]+)*(--|__|$)?',
+      'componentSelectors': function(fileName, presetOptions) {
+        const WORD = '([a-z]+(-[a-z]+)*)';
+        const SCSS_INTERPOLATION = '(.*#{.*}.*)';
+
+        const ns = (presetOptions && presetOptions.namespace) ? `${ presetOptions.namespace }-` : '';
+
+        const block = fileName.match(/^([a-z]+(-[a-z]+)*)+?/g);
+        const element = `(?:--${ WORD })?`;
+        const modifier = `(?:__${ WORD })?`;
+        const attribute = '(?:\\[.+\\])?';
+        const bemSelector = `(\\w)*(\\.${ns}${ block }${ element }${ modifier }${ attribute }|${ SCSS_INTERPOLATION })*`;
+
+        return new RegExp(`^(${ bemSelector })$`);
+      },
+      'ignoreSelectors': [
+        '\\.(u|trs)-([a-z]+(-[a-z]+)*)',
+        '^(html|body|&)',
+      ],
+    },
+  },
+};
---
+++ README.md
@@ -0,0 +1,12 @@
+# stylelint-config-bem-pattern
+dreipol stylelint config file for bem selectors
+
+## Installation
+
+```shell
+npm i "@dreipol/stylelint-config-bem-pattern -D
+```
+
+## Usage
+
+Add `"extends": "@dreipol/stylelint-config-bem-pattern"` to your `.stylelintrc` file
